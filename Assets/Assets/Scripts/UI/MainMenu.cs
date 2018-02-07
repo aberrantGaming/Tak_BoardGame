c3a6cd5e84eb3b424a3a62328761562a18059ae0
aberrantGeek <RyanCortino@gmail.com>
2018-02-07 12:30:30 -0600

    Created MainMenu scene and added basic UI elements.

---
+++ Assets/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        TMPro.TMP_Dropdown Selection = gameObject.GetComponentInChildren<TMPro.TMP_Dropdown>();
+
+        if (Selection.value == 0)
+            Utils.SelectGameboard("Beginner");
+        else if (Selection.value == 2)
+            Utils.SelectGameboard("Master");
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
