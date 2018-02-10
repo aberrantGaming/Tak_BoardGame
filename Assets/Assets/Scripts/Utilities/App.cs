6e1756ec388aa80106499f5c6814358b5f4bc129
aberrantGeek <RyanCortino@gmail.com>
2018-02-10 00:26:03 -0600

    Added _preload scene and associated utilities.

---
+++ Assets/Assets/Scripts/Utilities/App.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class App : MonoBehaviour
+{
+	void Start () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
