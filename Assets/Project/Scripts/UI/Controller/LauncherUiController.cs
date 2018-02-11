0c6edcf801aa72c6ef18ece1dc9864b9a7457f72
aberrantGeek <RyanCortino@gmail.com>
2018-02-11 10:05:31 -0600

    Cleaned up project directories.

---
+++ Assets/Project/Scripts/UI/Controller/LauncherUiController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.SeeSameGames.Tak
+{
+    public class LauncherUiController : UiController
+    {
+
+    }
+}
