28224c70d432359418af45de782b82364f6447d0
aberrantGeek <RyanCortino@gmail.com>
2018-02-15 06:36:27 -0600

    Polished formatting in PickupStack.cs

--- Assets/Project/Scripts/GameCommands/PickupStack.cs
+++ Assets/Project/Scripts/GameCommands/PickupStack.cs
@@ -1,34 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Com.SeeSameGames.Tak
 {
     public class PickupStack : ICommand
     {
+        #region Public Properties
+
         public Tile Source { get; private set; }
         public StoneColor RequestingPlayer { get; private set; }
         public int CarryLimit { get; private set; }
 
-        public bool IsCompleted { get; set; }
+        #endregion
+
+        #region Private variables
 
         private PlayerManager pm;
 
-        public bool IsLegal
-        {
-            get
-            {
-                if (Source.Stack.Count == 0)                            // don't request a nonexistent stack
-                    return false;
+        #endregion
 
-                if (Source.ControllingPlayer != null 
-                    && RequestingPlayer != Source.ControllingPlayer)    // don't request an opponent's stack
-                    return false;
-
-                return true;
-            }
-            private set { }
-        }
+        #region Constructor
 
         public PickupStack(Tile source, StoneColor requestingPlayer, int carryLimit)
         {
@@ -40,15 +31,24 @@ namespace Com.SeeSameGames.Tak
 
             IsCompleted = false;
         }
-        
+
+        #endregion
+
+        #region Interface Members
+
+        public bool IsCompleted { get; set; }
+
+        /// <summary>
+        /// Allow a player, who controls this tile, to pickup a number of pieces from the top of the stack not exceeding their carry limit.
+        /// </summary>
         public void Execute()
         {
-            if (!IsLegal)
+            if (!Allowed)
                 return;
 
             Queue<Stone> requestedHand = new Queue<Stone>();
             requestedHand.Clear();
-            
+
             for (int i = CarryLimit; i < 0; i--)
             {
                 requestedHand.Enqueue(Source.Stack[Source.Stack.Count - i]);
@@ -60,5 +60,27 @@ namespace Com.SeeSameGames.Tak
 
             IsCompleted = true;
         }
+
+        #endregion
+
+        #region Public Methods
+
+        public bool Allowed
+        {
+            get
+            {
+                if (Source.Stack.Count == 0)                            // don't request a nonexistent stack
+                    return false;
+
+                if (Source.ControllingPlayer != null
+                    && RequestingPlayer != Source.ControllingPlayer)    // don't request an opponent's stack
+                    return false;
+
+                return true;
+            }
+            private set { }
+        }
+
+        #endregion
     }
 }
