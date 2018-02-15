242e13dc712ff5002e6f56cc8b61ee7f13e19442
aberrantGeek <RyanCortino@gmail.com>
2018-02-15 05:07:56 -0600

    First pass at a (functioning) PickupStack command.

---
+++ Assets/Project/Scripts/GameCommands/PickupStack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.SeeSameGames.Tak
+{
+    public class PickupStack : ICommand
+    {
+        public Tile Source { get; private set; }
+        public StoneColor RequestingPlayer { get; private set; }
+        public int CarryLimit { get; private set; }
+
+        public bool IsCompleted { get; set; }
+
+        private PlayerManager pm;
+
+        public bool IsLegal
+        {
+            get
+            {
+                return true;
+            }
+            private set { }
+        }
+
+        public PickupStack(Tile source, StoneColor requestingPlayer, int carryLimit)
+        {
+            pm = PlayerManager.Instance;
+
+            Source = source;
+            RequestingPlayer = requestingPlayer;
+            CarryLimit = carryLimit;
+
+            IsCompleted = false;
+        }
+        
+        public void Execute()
+        {
+            if (!IsLegal)
+                return;
+
+            Debug.Log("Player Picked Up Stack of " + CarryLimit + " stones.");
+
+            IsCompleted = true;
+        }
+    }
+}
