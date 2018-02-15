a118af362b0711d201bcfea07445038bb2c4b649
aberrantGeek <RyanCortino@gmail.com>
2018-02-15 05:07:07 -0600

    Implemented basic Command Pattern

---
+++ Assets/Project/Scripts/GameCommands/Command.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.SeeSameGames.Tak
+{
+    public interface ICommand
+    {
+        bool IsCompleted { get; set; }
+
+        void Execute();
+    }
+}
