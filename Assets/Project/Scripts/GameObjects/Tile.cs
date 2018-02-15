1732087fba2c432b9ca8a65c3247366d7320e4ba
aberrantGeek <RyanCortino@gmail.com>
2018-02-15 02:58:53 -0600

    Refactored Tile.cs to accomodate game rules

--- Assets/Project/Scripts/GameObjects/Tile.cs
+++ Assets/Project/Scripts/GameObjects/Tile.cs
@@ -11,23 +11,48 @@ namespace Com.SeeSameGames.Tak
         public int tileID;
 
         private Stone lastStoneAdded;
-
-        public Queue<Stone> Stack { get; private set; }
+                
+        public List<Stone> Stack { get; private set; }
 
         #region Properties
 
-        public bool IsBlocked
-        {
+        /// <summary>
+        /// The tile is controlled by the player who's tile was placed on it last.
+        /// </summary>
+        public StoneColor? ControllingPlayer {
             get {
-                if (lastStoneAdded != null && lastStoneAdded.Type != StoneType.FLAT_STONE)
-                        return true;
+                if (lastStoneAdded == null)
+                    return null;
 
-                return false;
+                return lastStoneAdded.StoneColor;
             }
-            private set
-            {
+            private set { }
+        }
 
+        /// <summary>
+        /// The tile is Insurmountable if the last stone on the stack is a CAP_STONE. 
+        /// </summary>
+        public bool IsInsurmountable {
+            get {
+                if (lastStoneAdded == null)
+                    return false;
+
+                return (lastStoneAdded.StoneType == StoneType.CAP_STONE);
             }
+            private set {  }
+        }
+
+        /// <summary>
+        /// The tile is a Wall if the last stone on the stack is in the STANDING orientation. 
+        /// </summary>
+        public bool IsAWall {
+            get {
+                if (lastStoneAdded == null)
+                    return false;
+
+                return (lastStoneAdded.StoneOrientation == StoneOrientations.STANDING);
+            }
+            private set { }
         }
 
         #endregion
@@ -36,20 +61,46 @@ namespace Com.SeeSameGames.Tak
 
         protected void Awake()
         {
-            Stack = new Queue<Stone>();
+            Stack = new List<Stone>();
         }
 
         #endregion
 
         #region Public Methods
 
-        public void AddStoneToStack(Stone stoneToAdd)
+        /// <summary>
+        /// Allow a player, who controls this tile, to pickup a number of pieces from the top of the stack equal to their carry limit.
+        /// </summary>
+        public Queue<Stone> PickupStack(StoneColor requestingPlayer, int carryLimit)
         {
-            if (lastStoneAdded.Type != StoneType.FLAT_STONE)
-                return;
+            if (requestingPlayer != ControllingPlayer)
+                return null;
 
-            Stack.Enqueue(stoneToAdd);
+            Queue<Stone> requestedHand = new Queue<Stone>();
+            requestedHand.Clear();
+
+            for (int i = carryLimit; i < 0; i--)
+            {
+                requestedHand.Enqueue(Stack[Stack.Count - i]);
+            }
+
+            return requestedHand;
         }
+
+        //public bool AddStoneToStack(Stone stoneToAdd)
+        //{
+        //    switch (stoneToAdd.StoneType)
+        //    {
+        //        case (StoneType.STONE):
+
+        //            break;
+
+        //        case (StoneType.CAP_STONE):
+        //            if (IsInsurmountable)
+        //                return false;
+        //            break;
+        //    }
+        //}
         
         #endregion
     }
