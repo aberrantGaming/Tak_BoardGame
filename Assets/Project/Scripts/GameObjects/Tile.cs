3b0f616125e92d6149c0a4691788128c6ead9d24
aberrantGeek <RyanCortino@gmail.com>
2018-02-16 19:33:48 -0600

    Added Tile Object.

---
+++ Assets/Project/Scripts/GameObjects/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.aberrantGames.Tak.GameEngine
+{
+    public class Tile : MonoBehaviour
+    {
+        string tileID;
+        List<Stone> Stack;
+    }
+}
