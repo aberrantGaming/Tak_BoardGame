700f8cea4fa7d751264e3357d1d7422cc304ffde
aberrantGeek <RyanCortino@gmail.com>
2018-02-16 18:18:24 -0600

    Refactored & Renamed DevPreload to DefaultSceneLoader

---
+++ Assets/Project/Scripts/Utilities/DefaultSceneLoader.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Com.SeeSameGames.Tak
+{
+    [InitializeOnLoad]
+    static class SceneAutoLoader
+    {
+
+        static string sceneToLoad = "Assets/Scenes/_preload.unity";
+        private static string PreviousScenePath
+        {
+
+            get
+            {
+
+                return EditorPrefs.GetString("SceneAutoLoader.PreviousScenePath", SceneManager.GetActiveScene().path);
+            }
+
+            set
+            {
+
+                EditorPrefs.SetString("SceneAutoLoader.PreviousScenePath", value);
+            }
+        }
+
+        static SceneAutoLoader()
+        {
+
+            EditorApplication.playmodeStateChanged += OnPlayModeChanged;
+        }
+
+        private static void OnPlayModeChanged()
+        {
+
+            if (!EditorApplication.isPlaying && EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+
+                if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                {
+
+                    PreviousScenePath = SceneManager.GetActiveScene().path;
+                    EditorSceneManager.OpenScene(sceneToLoad);
+                }
+
+                return;
+            }
+
+            if (!EditorApplication.isPlaying && !EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+
+                EditorSceneManager.OpenScene(PreviousScenePath);
+            }
+        }
+    }
+}
