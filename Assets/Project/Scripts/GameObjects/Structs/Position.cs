de53525ef44165a3fd5e803c8764d249e791e9d2
aberrantGeek <RyanCortino@gmail.com>
2018-02-17 10:25:38 -0600

    Added Position struct asset.

---
+++ Assets/Project/Scripts/GameObjects/Structs/Position.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.aberrantGames.Tak.GameEngine
+{
+    public struct Position
+    {
+        public Config cfg;
+        public byte WhiteStones;
+        public byte WhiteCapstones;
+        public byte BlackStones;
+        public byte BlackCapstones;
+
+        public int move;
+    }
+}
