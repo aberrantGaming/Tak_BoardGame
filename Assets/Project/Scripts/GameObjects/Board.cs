6bd56559f1c339254df98e9ecfec6802e7008899
aberrantGeek <RyanCortino@gmail.com>
2018-02-18 12:56:58 -0600

    Created class for the Board object. Redelegated responsibilites from GameHolder to Board.

--- Assets/Project/Scripts/GameObjects/Board.cs
+++ Assets/Project/Scripts/GameObjects/Board.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 namespace Com.aberrantGames.Tak.GameEngine
 {
-    public class Board : MonoBehaviour
+    public class Board
     {
+        public GameHolder cfg;
 
+        public List<Tile> BoardState { get; private set; }
+
+        List<int> defaultStones = new List<int> { 0, 0, 0, 10, 15, 21, 30, 40, 50 };
+        List<int> defaultCapstones = new List<int> { 0, 0, 0, 0, 0, 1, 1, 1, 2 };
+
+        public Board(GameHolder g)
+        {
+            Debug.Log("Board Size : " + g.BoardSize);
+
+            if (g.StonesCount == 0)
+                g.SetStonesCount(defaultStones[g.BoardSize]);
+
+            if (g.CapstonesCount == 0)
+                g.SetCapstonesCount(defaultCapstones[g.BoardSize]);
+
+            BoardState = new List<Tile>();
+
+            int numberOfTiles = g.BoardSize * g.BoardSize;
+            for (int i = 0; i < numberOfTiles; i++)
+            {
+                this.BoardState.Add(new Tile());
+            }
+        }
     }
 
 }
--- Assets/Project/Scripts/GameObjects/ScriptableObjects/GameHolder.cs
+++ Assets/Project/Scripts/GameObjects/ScriptableObjects/GameHolder.cs
@@ -17,21 +17,18 @@ namespace Com.aberrantGames.Tak.GameEngine
         [SerializeField] private byte playerCount;
         public byte PlayerCount { get { return playerCount; } private set { } }
 
+        [SerializeField] private bool blackWinsTies;
+        public bool BlackWinsTies { get { return blackWinsTies; } private set { } }
+
         [SerializeField] private int stonesCount;
         public int StonesCount { get { return stonesCount; } private set { } }
 
         [SerializeField] private int capstonesCount;
         public int CapstonesCount { get { return capstonesCount; } private set { } }
-
-        [SerializeField] private bool blackWinsTies;
-        public bool BlackWinsTies { get { return blackWinsTies; } private set { } }
-
+        
         #endregion
 
         #region Private Variables
-                
-        List<int> defaultStones = new List<int> { 0, 0, 0, 10, 15, 21, 30, 40, 50 };
-        List<int> defaultCapstones = new List<int> { 0, 0, 0, 0, 0, 1, 1, 1, 2 };
 
         List<Tile> BoardTiles;
 
@@ -45,26 +42,35 @@ namespace Com.aberrantGames.Tak.GameEngine
 
             this.GamemodeName = "Default";
 
-            this.boardSize = 5;
+            this.boardSize = 3;
             this.playerCount = 1;
             this.blackWinsTies = true;
-            this.stonesCount = defaultStones[boardSize];
-            this.capstonesCount = defaultCapstones[boardSize];
+            this.stonesCount = 0;
+            this.capstonesCount = 0;
 
             this.BoardTiles = new List<Tile>();
-
-            int numberOfTiles = this.BoardSize * this.BoardSize;
-            for (int i = 0; i < numberOfTiles; i++)
-            {
-                this.BoardTiles.Add(new Tile());
-            }
         }
-        
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetStonesCount(int c)
+        {
+            stonesCount = c;
+        }
+
+        public void SetCapstonesCount(int c)
+        {
+            capstonesCount = c;
+        }
+
         #endregion
 
         // TODO: setup (object): Function that returns the initial value of G.
 
         // TODO: moves (object): The keys are move names, and the values are pure functions that return the new value of G once the move has been processed.
+        // Something like IDictionary<Move, void> MoveCallbacks;
 
         // TODO: flow (object): Arguments to customize the flow of the game.
 
