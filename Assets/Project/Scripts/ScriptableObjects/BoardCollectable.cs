228881fcc256758be36e2c1db2b6285b380db2bc
aberrantGeek <RyanCortino@gmail.com>
2018-02-18 20:58:41 -0600

    Removed illegal call to CreatePrimitive in ScriptableObject constructor

--- Assets/Project/Scripts/ScriptableObjects/BoardCollectable.cs
+++ Assets/Project/Scripts/ScriptableObjects/BoardCollectable.cs
@@ -19,21 +19,6 @@ namespace Com.aberrantGames.Tak.Collectables
         {
             this.BoardName = "Development Board";
             this.BoardDesc = "Run-time board for development use.";
-
-            this.BoardFoundation =
-                GameObject.CreatePrimitive(PrimitiveType.Cube);
-            BoardFoundation.transform.localScale = new Vector3(1.0f, 0.01f, 1.0f);
-            BoardFoundation.transform.position = new Vector3(0, 0, 0);
-
-            this.TileDark =
-                GameObject.CreatePrimitive(PrimitiveType.Cube);
-            TileDark.transform.localScale = new Vector3(0.95f, 0.1f, 0.95f);
-            TileDark.transform.position = new Vector3(0, 0.1f, 0);
-
-            this.TileLight =
-                GameObject.CreatePrimitive(PrimitiveType.Cube);
-            TileLight.transform.localScale = new Vector3(0.95f, 0.1f, 0.95f);
-            TileLight.transform.position = new Vector3(0, 0.1f, 0);
         }
 
     }
