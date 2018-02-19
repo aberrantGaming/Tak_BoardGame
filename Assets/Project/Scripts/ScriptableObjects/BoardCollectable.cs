2f8520813e590b1d945cf7788dc7761763529493
aberrantGeek <RyanCortino@gmail.com>
2018-02-18 20:22:42 -0600

    Added constructor class to BoardCollectable

--- Assets/Project/Scripts/ScriptableObjects/BoardCollectable.cs
+++ Assets/Project/Scripts/ScriptableObjects/BoardCollectable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Com.aberrantGames.Tak.Collectables
 {
+    [CreateAssetMenu(menuName = "Collectable/BoardCollectable")]
     public class BoardCollectable : ScriptableObject
     {
         public string BoardName;
@@ -13,5 +14,27 @@ namespace Com.aberrantGames.Tak.Collectables
 
         public GameObject TileDark;
         public GameObject TileLight;
+
+        public BoardCollectable()
+        {
+            this.BoardName = "Development Board";
+            this.BoardDesc = "Run-time board for development use.";
+
+            this.BoardFoundation =
+                GameObject.CreatePrimitive(PrimitiveType.Cube);
+            BoardFoundation.transform.localScale = new Vector3(1.0f, 0.01f, 1.0f);
+            BoardFoundation.transform.position = new Vector3(0, 0, 0);
+
+            this.TileDark =
+                GameObject.CreatePrimitive(PrimitiveType.Cube);
+            TileDark.transform.localScale = new Vector3(0.95f, 0.1f, 0.95f);
+            TileDark.transform.position = new Vector3(0, 0.1f, 0);
+
+            this.TileLight =
+                GameObject.CreatePrimitive(PrimitiveType.Cube);
+            TileLight.transform.localScale = new Vector3(0.95f, 0.1f, 0.95f);
+            TileLight.transform.position = new Vector3(0, 0.1f, 0);
+        }
+
     }
 }
