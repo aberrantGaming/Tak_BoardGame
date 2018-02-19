c6edeae1d0dac92c75d550c4e5b78b91338605fd
aberrantGeek <RyanCortino@gmail.com>
2018-02-19 15:51:45 -0600

    Added regions to board.cs

--- Assets/Project/Scripts/GameObjects/Board.cs
+++ Assets/Project/Scripts/GameObjects/Board.cs
@@ -6,18 +6,28 @@ namespace Com.aberrantGames.Tak.GameEngine
 {
     public class Board
     {
+        #region Public Variables
+
         public GameHolder config = new GameHolder();
 
         public List<Tile> BoardState;// { get; private set; }
 
+        #endregion
+
+        #region Private Variables
+
         List<int> defaultStones = new List<int> { 0, 0, 0, 10, 15, 21, 30, 40, 50 };
         List<int> defaultCapstones = new List<int> { 0, 0, 0, 0, 0, 1, 1, 1, 2 };
+
+        #endregion
+
+        #region Constructor
 
         public Board(GameHolder g)
         {
             if (g != null)
                 config = g;
-            
+
             if (config.StonesCount == 0)
                 config.SetStonesCount(defaultStones[config.BoardSize]);
 
@@ -32,6 +42,9 @@ namespace Com.aberrantGames.Tak.GameEngine
                 this.BoardState.Add(new Tile());
             }
         }
+
+        #endregion
+
     }
 
 }
