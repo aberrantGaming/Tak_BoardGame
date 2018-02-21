21c1374bdb104c5d35e0106f7c384f4c36082bb4
aberrantGeek <RyanCortino@gmail.com>
2018-02-20 18:43:50 -0600

    Relocated Slide struct. Added placeholder method for Len().

---
+++ Assets/Project/Scripts/GameObjects/Structs/Slide.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.aberrantGames.Tak.GameEngine
+{
+    public struct Slide
+    {
+        public int Len()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
