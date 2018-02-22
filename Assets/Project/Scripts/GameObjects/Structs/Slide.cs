dd960f511981ec0c37e1435c0de948988557f500
aberrantGeek <RyanCortino@gmail.com>
2018-02-22 10:28:59 -0600

    Refactored (and hopefully finished) Slide struct

--- Assets/Project/Scripts/GameObjects/Structs/Slide.cs
+++ Assets/Project/Scripts/GameObjects/Structs/Slide.cs
@@ -3,45 +3,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Slides = System.Int32;
+using SlideIterator = System.Int32;
+
 namespace Com.aberrantGames.Tak.GameEngine
 {
-    //public struct SlideIterator
-    //{
-    //    public Slide Slide { get; private set; }
-
-    //    public SlideIterator(Slide _slide)
-    //    {
-    //        Slide = _slide;
-    //    }
-
-    //    public bool Ok { get { return (Slide.Len() < 4); } private set { } }
-
-    //    public SlideIterator Next()
-    //    {
-
-    //    }        
-    //}
-
-    public class Slides
+    /// <summary>
+    ///         Slides is essentially a packed [8]uint4, used to represent the
+    ///     slide counts in a Tak move in a space-efficient way. We store the
+    ///     first drop count in (s&0xf), the next in (s&0xf0), and so on.
+    /// </summary>
+    public struct Slide
     {
-        public List<int> Slide { get; private set; }
+        private Slides s;
+        private SlideIterator sI;
 
-        public Slides(List<int> _drops)
-        {
-            for (int i = _drops.Count; i <= 0; i--)
+        public Slides MkSlides(params int[] _drops) {
+            Slides retVal = new Slides();
+
+            for (int i = _drops.Length; i >= 0; i--)
             {
                 if (_drops[i] > 8)
                     Debug.LogError("bad drop");
 
-                Slide.Add(_drops[i]);
+                retVal = Prepend(_drops[i]);
             }
 
+            return retVal;
         }
 
-        public int Length { get { return Slide.Count; } private set { } }
-        public bool IsEmpty { get { return Slide.Count.Equals(0); } private set { } }
+        #region Slide Methods
 
+        public int Len()
+        {
+            int l = 0;
+            while (s != 0)
+            {                // TO DO : Investigate chances of infinite loop
+                l++;
+                s >>= 4;
+            };
+            return l;
+        }
 
+        public bool Empty()
+        {
+            return s == 0;
+        }
+
+        public bool Singleton()
+        {
+            return s > 0xf;
+        }
+
+        public int First()
+        {
+            return (int)s & 0xf;
+        }
+
+        public Slides Prepend(int _next)
+        {
+            return (s << 4) | (Slides)_next;
+        }
+
+        #endregion
+
+        #region SlideIterator Methods
+
+        SlideIterator Iterator()
+        {
+            return (SlideIterator)s;
+        }
+
+        SlideIterator Next()
+        {
+            return s >> 4;
+        }
+
+        bool Ok()
+        {
+            return s != 0;
+        }
+
+        int Elem()
+        {
+            return (int)s & 0xf;
+        }
+
+        #endregion
     }
-
 }
