595c348cd7fb4a641f8da39725d4975f388c9a46
aberrantGeek <RyanCortino@gmail.com>
2018-02-28 13:43:19 -0600

    Completed first pass at Move.cs

--- Assets/Project/Scripts/Tak/Move.cs
+++ Assets/Project/Scripts/Tak/Move.cs
@@ -1,88 +1,34 @@
 ﻿using Com.aberrantGames.Tak.Utilities;
 using System.Collections.Generic;
 using UnityEngine;
+using Color = System.Byte;
 
 namespace Com.aberrantGames.Tak.GameEngine
 {
     public enum MoveError { ErrOccupied, ErrIllegalSlide, ErrNoCapstone, ErrIllegalOpening }
     public enum MoveType { Pass = 1, PlaceFlat, PlaceStanding, PlaceCapstone, SlideLeft, SlideRight, SlideUp, SlideDown }
 
+    struct Dircnt
+    {
+        public MoveType d;
+        public int c;
+    }
+
     public struct Move
     {
-        #region Variables
-
         public MoveType Type;
         public Slide Slide;
         public int X, Y;
-        
-        public static List<List<Slide>> possibleSlides;
 
-        #endregion
+        private List<List<Slide>> possibleSlides;
 
-        #region Properties
-
-        public bool IsSlide
+        public Move(int x, int y, MoveType type, Slide slide)
         {
-            get { return Type >= MoveType.SlideLeft; }
-            private set { }
-        }
+            X = x;
+            Y = y;
+            Type = type;
+            Slide = slide;
 
-        public CoordPair Dest
-        {
-            get
-            {
-                switch (Type)
-                {
-                    case MoveType.PlaceFlat:
-                    case MoveType.PlaceStanding:
-                    case MoveType.PlaceCapstone:
-                        return new CoordPair()
-                        {
-                            X = X,
-                            Y = Y
-                        };
-                    case MoveType.SlideLeft:
-                        return new CoordPair()
-                        {
-                            X = X - Slide.Len,
-                            Y = Y
-                        };
-                    case MoveType.SlideRight:
-                        return new CoordPair()
-                        {
-                            X = X + Slide.Len,
-                            Y = Y
-                        };
-                    case MoveType.SlideUp:
-                        return new CoordPair()
-                        {
-                            X = X,
-                            Y = Y + Slide.Len,
-                        };
-                    case MoveType.SlideDown:
-                        return new CoordPair()
-                        {
-                            X = X,
-                            Y = Y - Slide.Len,
-                        };
-                    default:
-                        Debug.LogError("bad type");
-                        return new CoordPair();
-                }
-            }
-        }
-
-        #endregion
-
-        #region Constructors
-
-        public Move(int _x, int _y, MoveType _type, Slide _slide)
-        {
-            X = _x;
-            Y = _y;
-            Type = _type;
-            Slide = _slide;
-            
             // initiate the list of possible slides
             possibleSlides = new List<List<Slide>>(10);
             for (int i = 0; i <= 8; i++)
@@ -90,24 +36,6 @@ namespace Com.aberrantGames.Tak.GameEngine
                 possibleSlides[i] = CalculateSlides(i);
             }
         }
-
-        #endregion
-
-        public bool Equal(Move _rhs)
-        {
-            if (X != _rhs.X || Y != _rhs.Y)
-                return false;
-            if (Type != _rhs.Type)
-                return false;
-            if (!IsSlide)
-                return true;
-            if (!Slide.Equals(_rhs.Slide))
-                return false;
-
-            return true;
-        }
-        
-        #region Private Methods
 
         private List<Slide> CalculateSlides(int _stack)
         {
@@ -123,10 +51,140 @@ namespace Com.aberrantGames.Tak.GameEngine
                     }
                 }
             }
-
             return retVal;
+        }
+    }
+
+    public static class MoveHelper
+    {
+        #region Extension Properties
+
+        public static CoordPair GetDest(this Move m)
+        {
+            switch (m.Type)
+            {
+                case MoveType.PlaceFlat:
+                case MoveType.PlaceStanding:
+                case MoveType.PlaceCapstone:
+                    return new CoordPair()
+                    {
+                        X = m.X,
+                        Y = m.Y
+                    };
+                case MoveType.SlideLeft:
+                    return new CoordPair()
+                    {
+                        X = m.X - m.Slide.Len,
+                        Y = m.Y
+                    };
+                case MoveType.SlideRight:
+                    return new CoordPair()
+                    {
+                        X = m.X + m.Slide.Len,
+                        Y = m.Y
+                    };
+                case MoveType.SlideUp:
+                    return new CoordPair()
+                    {
+                        X = m.X,
+                        Y = m.Y + m.Slide.Len,
+                    };
+                case MoveType.SlideDown:
+                    return new CoordPair()
+                    {
+                        X = m.X,
+                        Y = m.Y - m.Slide.Len,
+                    };
+                default:
+                    Debug.LogError("bad type");
+                    return new CoordPair();
+            }
+        }
+
+        public static bool IsSlide(this Move m)
+        {
+            return m.Type >= MoveType.SlideLeft;
+        }
+
+        public static bool Equal(this Move m, Move _rhs)
+        {
+            if (m.X != _rhs.X || m.Y != _rhs.Y)
+                return false;
+            if (m.Type != _rhs.Type)
+                return false;
+            if (!m.IsSlide())
+                return true;
+            if (!m.Slide.Equals(_rhs.Slide))
+                return false;
+
+            return true;
+        }
+
+        #region Extension Methods
+
+        public static List<Move> AllMoves(this Position p, List<Move> _moves)
+        {
+            Color next = p.ToMove();
+            bool cap = false;
+
+            if (next == p.White)
+                cap = p.WhiteCapstones > 0;
+            else
+                cap = p.BlackCapstones > 0;
+
+            for (int x = 0; x < p.cfg.Size; x++)
+            {
+                for (int y = 0; y < p.cfg.Size; y++)
+                {
+                    uint i = (uint)(y * p.cfg.Size + x);
+
+                    if (p.Height[i] == 0)
+                    {
+                        _moves.Add(new Move() { X = x, Y = y, Type = MoveType.PlaceFlat });
+                        if (p.turn >= 2)
+                        {
+                            _moves.Add(new Move() { X = x, Y = y, Type = MoveType.PlaceStanding });
+                            if (cap)
+                                _moves.Add(new Move() { X = x, Y = y, Type = MoveType.PlaceCapstone });
+                        }
+                        continue;
+                    }
+
+                    if (p.turn < 2)
+                        continue;
+
+                    if ((next == Stone.White) && (p.White & (uint)(1 << (int)i)) == 0)
+                        continue;
+                    else if ((next == Stone.Black) && (p.Black & (uint)(1 << (int)i)) == 0)
+                        continue;
+
+                    Dircnt[] dirs = new Dircnt[4];
+                    dirs[0] = new Dircnt() { d = MoveType.SlideLeft, c = x };
+                    dirs[0] = new Dircnt() { d = MoveType.SlideRight, c = x - 1 };
+                    dirs[0] = new Dircnt() { d = MoveType.SlideDown, c = y };
+                    dirs[0] = new Dircnt() { d = MoveType.SlideUp, c = y - 1 };
+
+                    for (int d = 0; d < dirs.Length; d++)
+                    {
+                        uint h = p.Height[i];
+                        if (h > (uint)p.cfg.Size)
+                            h = (uint)p.cfg.Size;
+
+                        Slide mask = new Slide((1 << (4 * (int)dirs[d].c) - 1));
+
+                        //foreach (Slide s in slides[(int)h])
+                        //{
+                        //    if (s == mask)
+                        //        _moves.Add(new GameEngine.Move() { X = x, Y = y, Type = dirs[d].d, Slide = s });
+                        //}
+                    }
+                }
+            }
+            return _moves;
         }
 
         #endregion
+        #endregion
     }
 }
+
