9dead416a8529db371c8a82dbff53f55acc34305
aberrantGeek <RyanCortino@gmail.com>
2018-03-08 05:10:59 -0600

    Refactored DevGameScene scripts

---
+++ Assets/Project/Scripts/SceneManagers/Scene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.aberrantGames.Tak
+{
+    public class Scene : MonoBehaviour
+    {
+        GameManager gm;
+
+        void Awake()
+        {
+            gm = GameManager.Instance;
+            gm.OnStateChange += Gm_OnStateChange;
+        }
+
+        private void Gm_OnStateChange()
+        {
+            switch (gm.GameState)
+            {
+                case (GameState.IN_MATCH): gm.StartMatch(); break;
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            gm.SetGameState(GameState.IN_MATCH);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
